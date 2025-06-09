50989075b90f62717bd823cd5d42124e859fe931
Divyanshu Shekhar <149070310+Divyanshu832@users.noreply.github.com>
2025-06-10 01:49:49 +0530

    u

--- middleware.ts
+++ middleware.ts
@@ -19,6 +19,7 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
+    secureCookie: true
   });
 
   // If no token and trying to access protected routes, redirect to login
